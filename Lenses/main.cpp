fa21437d7dd659c85c05ebe54dc8ec5deed32e96
Jonathan Sterling <jonsterling@me.com>
2011-07-29 18:36:36 -0700

    Initial Commit

---
+++ Lenses/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lenses
+//
+//  Created by Jonathan Sterling on 7/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
