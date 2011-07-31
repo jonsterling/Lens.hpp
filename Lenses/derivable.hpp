875b425d32a12719b74dc5553278db62d5e5b4f7
Jonathan Sterling <jonsterling@me.com>
2011-07-31 13:32:51 -0700

    Fixed copyrights

--- Lenses/derivable.hpp
+++ Lenses/derivable.hpp
@@ -3,7 +3,7 @@
 //  Lenses
 //
 //  Created by Jonathan Sterling on 7/31/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Jonathan Sterling. All rights reserved.
 //
 
 #ifndef Lenses_derivable_hpp
--- Lenses/lens.hpp
+++ Lenses/lens.hpp
@@ -3,7 +3,7 @@
 //  Lenses
 //
 //  Created by Jonathan Sterling on 7/29/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Jonathan Sterling. All rights reserved.
 //
 
 #ifndef Lenses_lens_hpp
